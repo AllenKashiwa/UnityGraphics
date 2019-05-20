d8543f7b08b3fb9a86c50ea1bc5cf61fc1f9bd54
Allen Kashiwa <allenkashiwa@gmail.com>
2019-05-20 23:16:46 +0800

    添加反射探针示例

---
+++ GraphicProject/Assets/Common/SimpleHorizontalMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SimpleHorizontalMove : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if (h != 0f)
+        {
+            transform.position += speed * transform.right * h * Time.deltaTime;
+        }
+    }
+}
